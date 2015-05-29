ba04d62e1a8cc18bf0a28368702b0c23f575d31f
Gus Silva <gus7silva@yahoo.com>
2015-05-29 10:51:35 -0700

    Re did compress function

--- src/Tester.java
+++ src/Tester.java
@@ -14,78 +14,8 @@ public class Tester {
         String inFile = "files/input.txt";
         String compressedFile = "files/compressed.txt";
         String decompressedFile = "files/decompressed.txt";
-        //ZLcompress(inFile, compressedFile);
+        compress(inFile, compressedFile);
         //ZLdecompress(compressedFile, decompressedFile);
-    }
-
-    public static boolean ZLcompress(String inFilePath, String outFilePath)
-    {
-        BufferedReader reader;
-        BufferedWriter writer;
-        try {
-            //Prepare Input File
-            reader = new BufferedReader(new FileReader(inFilePath));
-            //Prepare Output File
-            writer = new BufferedWriter(new FileWriter(outFilePath, false));
-
-            /**
-             * We have chosen to initialize the dictionary with only letters A-Z
-             * for simplicity. However, we acknowledge that industrial strength
-             * code would initialize dictionary with entire ASCII table.
-             * */
-            String[] Dictionary = new String[255];
-            Hashtable<String, Integer> dict = new Hashtable<>(255);
-            for(int i = 0; i < 26; i++)
-            {
-                String temp = (char)(i+65)+"";
-                Dictionary[i] = temp;
-                dict.put(temp, i);
-
-            }
-
-            //Read input and generate compressed output
-            String curr;
-            String temp;
-            Integer outTemp;
-            while((curr = reader.readLine()) != null)
-            { //Read every line of file
-                for(int i = 0; i < curr.length(); i++)
-                { //Cycle through each character on that line
-                    temp = curr.charAt(i) + "";
-
-                    int j = 1;
-                    outTemp = null;
-                    while((dict.get(temp) != null) && ((i+j) < curr.length()))
-                    {
-                        outTemp = dict.get(temp);
-                        //System.out.println(temp + " FOUND IN DICT");
-                        temp += curr.charAt(i + j);
-                        j++;
-                    }
-                    dict.put(temp, 26 + i);
-                    if(outTemp != null) {
-                        System.out.println("Writing: " + outTemp);
-                        writer.write(outTemp + " ");
-                    }
-                }
-            }
-
-            //print dictionary
-            System.out.println(dict.toString());
-            reader.close();
-            writer.close();
-            return true;
-        }
-        catch( FileNotFoundException e)
-        {
-            System.out.println("Input file not found. Exiting.");
-            return false;
-        }
-        catch (IOException e)
-        {
-            System.out.println("Error creating output file. Exiting.");
-            return false;
-        }
     }
 
     public static boolean ZLdecompress(String inFilePath, String outFilePath)
@@ -105,36 +35,185 @@ public class Tester {
              * for simplicity. However, we acknowledge that industrial strength
              * code would initialize dictionary with entire ASCII table.
              * */
-            String[] Dictionary = new String[255];
-            Hashtable<Integer, String> dict = new Hashtable<>(255);
+            ArrayList<String> dictionary = new ArrayList<>();
             for(int i = 0; i < 26; i++)
             {
                 String temp = (char)(i+65)+"";
-                Dictionary[i] = temp;
-                dict.put(i, temp);
-
+                dictionary.add(temp);
             }
 
-            //Read input and generate compressed output
-            String curr = "";
-            int temp;
-            String outTemp;
-            while(in.hasNextInt())
-            { //Read every line of file
-                temp = in.nextInt();
-                outTemp = dict.get(temp);
+            //Read input and generate decompressed output
+            String curr, outTemp, temp;
+            String[] line;
+            Integer[] ints;
 
-                if(outTemp != null)
-                {
-                    curr += outTemp;
-                    System.out.println("Writing: " + outTemp);
-                    writer.write(outTemp + "\n");
+            while(in.hasNextLine())
+            { //Read every line of file
+                curr = in.nextLine();                   // Read in next line of input
+                line = curr.split(" ");                 // Convert input into an array
+                ints = new Integer[line.length];
+                for(int i = 0; i < ints.length; i++ )   // Parse Strings in array into Ints
+                    ints[i] = Integer.parseInt(line[i]);// and transfer to Integer array
+
+                for(int i = 0; i < ints.length; i++)
+                { //Cycle through each character on that line
+
+                    temp = dictionary.get(ints[i]);
+
+                    int j = 1;
+                    outTemp = null;
+                    while((dictionary.contains(temp)) && ((i+j) < ints.length))
+                    {
+                        outTemp = dictionary.get(dictionary.indexOf(temp));
+                        //System.out.println(temp + " FOUND IN DICT");
+                        temp += dictionary.get(ints[i + j]);
+                        j++;
+                    }
+                    dictionary.add(temp);
+                    if(outTemp != null) {
+                        System.out.println("Writing: " + temp);
+                        writer.write(outTemp + "\n");
+                    }
                 }
 
             }
 
             //print dictionary
-            System.out.println(dict.toString());
+            System.out.println(dictionary.toString());
+            reader.close();
+            writer.close();
+            return true;
+        }
+        catch( FileNotFoundException e)
+        {
+            System.out.println("Input file not found. Exiting.");
+            return false;
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error creating output file. Exiting.");
+            return false;
+        }
+    }
+
+    public static boolean compress(String inFilePath, String outFilePath)
+    {
+        BufferedReader reader;
+        BufferedWriter writer;
+        try {
+            //Prepare Input File
+            reader = new BufferedReader(new FileReader(inFilePath));
+            //Prepare Output File
+            writer = new BufferedWriter(new FileWriter(outFilePath, false));
+
+            /**
+             * We have chosen to initialize the dictionary with only letters A-Z
+             * for simplicity. However, we acknowledge that industrial strength
+             * code would initialize dictionary with entire ASCII table.
+             * */
+            ArrayList<String> dictionary = new ArrayList<>(255);
+            for(int i = 0; i < 26; i++)
+            {
+                String temp = (char)(i+65)+"";
+                dictionary.add(temp);
+            }
+
+            //Read input and generate compressed output
+            String P, C, line;
+            Integer outTemp;
+            while((line = reader.readLine()) != null)
+            {
+                int inc = 0;
+                while( inc < line.length())
+                {
+                    P = line.charAt(inc++) + "";
+                    C = line.charAt(inc) + "";
+
+                    while(dictionary.contains(P+C))
+                    {
+                        P = P+C;
+                        inc++;
+                        if(inc < line.length())
+                            C = line.charAt(inc) + "";
+                    }
+
+                    dictionary.add(P+C);
+                    outTemp = dictionary.indexOf(P);
+
+                    System.out.println("Writing: " + dictionary.get(outTemp));
+                    writer.write(outTemp + " = " + dictionary.get(outTemp) + "\n");
+                }
+            }
+
+            //print dictionary
+            System.out.println(dictionary.toString());
+            reader.close();
+            writer.close();
+            return true;
+        }
+        catch( FileNotFoundException e)
+        {
+            System.out.println("Input file not found. Exiting.");
+            return false;
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error creating output file. Exiting.");
+            return false;
+        }
+    }
+
+    public static boolean decompress(String inFilePath, String outFilePath)
+    {
+        BufferedReader reader;
+        BufferedWriter writer;
+        try {
+            //Prepare Input File
+            reader = new BufferedReader(new FileReader(inFilePath));
+            //Prepare Output File
+            writer = new BufferedWriter(new FileWriter(outFilePath, false));
+
+            /**
+             * We have chosen to initialize the dictionary with only letters A-Z
+             * for simplicity. However, we acknowledge that industrial strength
+             * code would initialize dictionary with entire ASCII table.
+             * */
+            ArrayList<String> dictionary = new ArrayList<>(255);
+            for(int i = 0; i < 26; i++)
+            {
+                String temp = (char)(i+65)+"";
+                dictionary.add(temp);
+            }
+
+            //Read input and generate compressed output
+            String P, C, line;
+            Integer outTemp;
+            while((line = reader.readLine()) != null)
+            {
+                int inc = 0;
+                while( inc < line.length())
+                {
+                    P = line.charAt(inc++) + "";
+                    C = line.charAt(inc) + "";
+
+                    while(dictionary.contains(P+C))
+                    {
+                        P = P+C;
+                        inc++;
+                        if(inc < line.length())
+                            C = line.charAt(inc) + "";
+                    }
+
+                    dictionary.add(P+C);
+                    outTemp = dictionary.indexOf(P);
+
+                    System.out.println("Writing: " + dictionary.get(outTemp));
+                    writer.write(outTemp + " = " + dictionary.get(outTemp) + "\n");
+                }
+            }
+
+            //print dictionary
+            System.out.println(dictionary.toString());
             reader.close();
             writer.close();
             return true;
